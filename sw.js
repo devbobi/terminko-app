579bb4d097849c0ec73fa320563a98833c9cfc08
devbobi <tobiasgabrielsanchez@gmail.com>
2026-07-16 22:37:41 +0200

    Prenova PWA: izbira kraja, jasni filtri, lepsi izgled

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Terminko PWA service worker - cache app shell, API vedno svez (network-first).
-const CACHE = "terminko-v1";
+const CACHE = "terminko-v2";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
